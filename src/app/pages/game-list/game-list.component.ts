cf0a94fb90965ffafe3b902cb9bbe2205f21392a
julia Dias <jedl@cin.ufpe.br>
2024-02-27 01:57:51 -0300

    feat: list game names

--- src/app/pages/game-list/game-list.component.ts
+++ src/app/pages/game-list/game-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GamesService } from '../../services/games/games.service';
+import { Game } from '../../interfaces/game';
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
-  styleUrl: './game-list.component.scss'
+  styleUrl: './game-list.component.scss',
 })
-export class GameListComponent {
+export class GameListComponent implements OnInit {
+  constructor(private gameService: GamesService) {}
 
+  public games: Game[] = [];
+
+  ngOnInit() {
+    this.gameService.getAllGames().subscribe((data: Game[]) => {
+      this.games = data;
+    });
+  }
 }
