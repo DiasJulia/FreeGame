5cc41bb87bdf6e4f2403968cb75f2dffcefa4683
julia Dias <jedl@cin.ufpe.br>
2024-02-26 23:53:44 -0300

    Initial Commit
    	new file:   .editorconfig
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   .vscode/launch.json
    	new file:   .vscode/tasks.json
    	new file:   README.md
    	new file:   angular.json
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   proxy/server.js
    	new file:   src/app/app-routing.module.ts
    	new file:   src/app/app.component.html
    	new file:   src/app/app.component.scss
    	new file:   src/app/app.component.spec.ts
    	new file:   src/app/app.component.ts
    	new file:   src/app/app.module.ts
    	new file:   src/app/list/list.module.ts
    	new file:   src/app/list/movies-listing/movies-listing.component.html
    	new file:   src/app/list/movies-listing/movies-listing.component.scss
    	new file:   src/app/list/movies-listing/movies-listing.component.spec.ts
    	new file:   src/app/list/movies-listing/movies-listing.component.ts
    	new file:   src/app/services/games/games.service.spec.ts
    	new file:   src/app/services/games/games.service.ts
    	new file:   src/assets/.gitkeep
    	new file:   src/favicon.ico
    	new file:   src/index.html
    	new file:   src/main.ts
    	new file:   src/styles.scss
    	new file:   tsconfig.app.json
    	new file:   tsconfig.json
    	new file:   tsconfig.spec.json

---
+++ src/app/services/games/games.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GamesService {
+  baseUrl = 'http://localhost:4123/https://www.freetogame.com/api/';
+
+  constructor(private http: HttpClient) {}
+
+  public getAllGames() {
+    return this.http.get(this.baseUrl + 'games');
+  }
+}
